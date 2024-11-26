9caa37925b0865d6c5db6d7780a4d7484c3e4e9c
elzhanxx <elzhan.maxanbet2004@mail.ru>
2024-11-26 18:01:22 +0500

    added

---
+++ src/Epic.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Epic extends Task {
+    private ArrayList<Subtask> subtasks;
+
+    public Epic(String title, String description) {
+        super(title, description);
+        subtasks = new ArrayList<>();
+    }
+}
